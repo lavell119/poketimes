3ee4636155ab44c6128867458df16325edf8e52e
Lee <leewhiz119@gmail.com>
2022-10-23 15:17:30 -0400

    added post component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
+import Post from './components/Post'
 
 
 class App extends Component {
@@ -16,11 +17,10 @@ class App extends Component {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />  
             <Route path="/contact" element={<Contact />} />
+            <Route path="/:posts_id" element={<Post />} />
           </Routes>      
         </div>
       </BrowserRouter>
-
-
     
     )
   }
