168a4abaa4714e4abcb98fee3e31222217099ded
Lee <leewhiz119@gmail.com>
2022-10-23 18:07:34 -0400

    installed redux and react-redux and created store and reducer function

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />  
             <Route path="/contact" element={<Contact />} />
-            <Route path="/:posts_id" element={<Post />} />
+            <Route path="/:post_id" element={<Post />} />
           </Routes>      
         </div>
       </BrowserRouter>
