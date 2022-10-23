763a8c934a8705798fcadf52f11b044c16b454d7
Lee <leewhiz119@gmail.com>
2022-10-22 23:37:47 -0400

    got routes working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Navbar from './components/Navbar'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
@@ -10,15 +10,18 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-        </Routes>
-      </div>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />  
+            <Route path="/contact" element={<Contact />} />
+          </Routes>      
+        </div>
       </BrowserRouter>
+
+
+    
     )
   }
 }
